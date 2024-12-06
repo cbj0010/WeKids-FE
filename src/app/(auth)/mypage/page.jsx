3998ffdea88e606855d7d0679cbcaa6c41df9b7b
안찬웅 <1stevering@naver.com>
2024-12-06 18:29:39 +0900

    [feat/#147]: 송금 비밀번호 구현

--- src/app/(auth)/mypage/page.jsx
+++ src/app/(auth)/mypage/page.jsx
@@ -1,5 +1,4 @@
 import Header from "@/src/ui/layout/Header";
-import Rectangle from "@/src/ui/components/atoms/Rectangle";
 import { SignOut } from "@/src/ui/components/auth/SignOut";
 import CustomButton from "@/src/ui/components/atoms/CustomButton";
 
@@ -10,14 +9,15 @@ export default function Page() {
         <Header />
       </div>
 
+      <div className="flex-grow"></div>
+
       <div className="flex flex-col items-center mt-4">
-        <Rectangle className="w-[343px] h-[77px] flex-shrink-0">
+        <div className="w-[343px] h-[77px] flex-shrink-0 rounded-[25px] bg-white shadow-md flex items-center justify-center">
           <div className="ml-4 text-B-16">송금 비밀번호 변경하기</div>
-        </Rectangle>
+        </div>
       </div>
 
       <div className="flex-grow"></div>
-
       <div className="flex flex-col justify-center items-center pb-10">
         <SignOut>
           <CustomButton size="mediumLarge" rounded={true}>
